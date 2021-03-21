5e00ee823ead1387ed40814f84adc7696a62f7ce
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-03-21 17:39:41 -0300

    update todo info

--- src/index.js
+++ src/index.js
@@ -69,7 +69,21 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params
+  const {user} = request
+
+  const {title, deadline} = request.body;
+
+  const todo = user.todos.find((todo) => todo.id == id)
+
+  if (!todo) {
+    return response.status(404).json({error: "todo not found"})
+  }
+
+  todo.title = title
+  todo.deadline = deadline
+
+  return response.json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
