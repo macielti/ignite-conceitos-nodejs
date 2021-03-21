1932fea2e56217259b60cc4caed0b143acd6e14c
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-03-21 17:43:36 -0300

    mark todo as done

--- src/index.js
+++ src/index.js
@@ -87,7 +87,19 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params
+  const {user} = request
+
+  const todo = user.todos.find((todo) => todo.id == id)
+
+  if (!todo) {
+    return response.status(404).json({error: "todo not found"})
+  }
+
+  todo.done = true
+
+  return response.json(todo)
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
